fcf7eba5425f508490dae17086ee751edba65665
Zack Scholl <zack.scholl@gmail.com>
2022-06-13 10:01:10 -0700

    update build

--- build.sh
+++ build.sh
@@ -1,23 +1,33 @@
 #!/bin/bash
 
+# get the modules
+git submodule update --init --recursive
+sudo apt install cmake
 
-#wget https://github.com/supercollider/supercollider/archive/refs/tags/Version-3.12.2.tar.gz
-#tar -xvzf Version-3.12.2.tar.gz
 
-# git submodule update --init --recursive
-# sudo apt install cmake
+# download SuperCollider source
+wget https://github.com/supercollider/supercollider/archive/refs/tags/Version-3.12.2.tar.gz
+tar -xvzf Version-3.12.2.tar.gz
 
+# build ported plugins
 # requires a fix to the dsp.h: https://github.com/electro-smith/DaisySP/issues/149
-# sed -i -e 's/__arm__/__armdisable__/g' portedplugins/DaisySP/Source/Utility/dsp.h
+sed -i -e 's/__arm__/__armdisable__/g' src/portedplugins/DaisySP/Source/Utility/dsp.h
+cd src/portedplugins
+rm -rf build
+mkdir -p build 
+cd build
+cmake .. -DCMAKE_BUILD_TYPE='Release' -DSC_PATH="/home/we/dust/code/supercollider-engines/supercollider-Version-3.12.2/" -DCMAKE_INSTALL_PREFIX="/home/we/dust/code/supercollider-engines/src/portedplugins/built/"
+cmake --build . --config Release
+cmake --build . --config Release --target install
+rm -rf PortedPlugins
+mv src/portedplugins/built/PortedPlugins .
 
-# cd portedplugins
-# rm -rf build
-# mkdir -p build 
-# cd build
-# cmake .. -DCMAKE_BUILD_TYPE='Release' -DSC_PATH="/home/we/dust/code/supercollider-engines/supercollider-Version-3.12.2/" -DCMAKE_INSTALL_PREFIX="/home/we/dust/code/supercollider-engines/built/"
-# cmake --build . --config Release
-# cmake --build . --config Release --target install
-
-cd /home/we/dust/code/supercollider-engines/mi-UGens
+# build mi-UGens
+cd /home/we/dust/code/supercollider-engines/src/mi-UGens
 chmod +x build.sh
 ./build.sh /home/we/dust/code/supercollider-engines/supercollider-Version-3.12.2/
+rm -rf mi-Ugens
+mv src/mi-UGens/build/mi-UGens .
+
+# clean up
+rm -rf supercollider-Version-3.12.2 Version-3.12.2.tar.gz
